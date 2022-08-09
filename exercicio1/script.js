a8556dc9bf6edd43bdf821bef7c23f4b935b60ea
Isaías Oliveira dos Reis <isaiasodr@gmail.com>
2022-08-09 11:11:35 -0300

    exercícios 1, 2 e 3 finalizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,7 @@
-const num = //seu numero aqui
+const num = Number(prompt("digite um número?"));
 
+if (num % 2 === 0 || num % 3 === 0) {
+  console.log("acertou");
+} else {
+  console.log("este número não é divisível por 2 ou 3");
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,22 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
+/* if (nome === "José") {
+  console.log("Oi, Zé!");
 } else {
-	console.log("Olá, "+nome);
+  console.log("Olá, " + nome);
 }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
+if (idade >= 18) {
+  console.log("pode tirar carteira de motorista!");
 } else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+  console.log("Ainda não pode tirar carteira de motorista");
+} */
+
+let idade = Number(prompt("qual a sua idade"));
+let nome = prompt("qual seu nome?");
+
+nome === "José" ? console.log("Oi zé!") : console.log(`Olá,${nome}`);
+
+idade >= 18
+  ? console.log("você pode tirar carteira!")
+  : console.log("você pode não tirar carteira!");
